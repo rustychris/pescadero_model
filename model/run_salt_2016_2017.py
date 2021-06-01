c73e5f9177360ef35cb771756d8f29af27ad0f6e
Rusty Holleman <rustychris@gmail.com>
2021-06-01 09:54:09 -0700

    Trying a salty IC

--- model/run_salt_2016_2017.py
+++ model/run_salt_2016_2017.py
@@ -11,7 +11,7 @@ from stompy.grid import unstructured_grid
 
 model=pesca_base.PescaButano(run_start=np.datetime64("2016-06-01 00:00"),
                              run_stop=np.datetime64("2016-06-30 12:00"),
-                             run_dir="run_salt_20160520-v63",
+                             run_dir="run_salt_20160520-v64",
                              salinity=True,
                              temperature=True,
                              nlayers_3d=28,
@@ -22,9 +22,12 @@ model=pesca_base.PescaButano(run_start=np.datetime64("2016-06-01 00:00"),
 # Diagnosing time step slowdown
 # model.mdu['time','Timestepanalysis']=1
 model.mdu['time','AutoTimestep']=4 # 5=bad. 4 okay but slower, seems no better than 3.
-model.mdu['numerics','TurbulenceModel']=1 # 0: dead run.  1: should be 5e-5.
-# model.mdu['numerics','Turbulenceadvection']=0 # had been 3, but maybe that's causing issues?
-model.mdu['physics','Vicoww']=5e-3 # 100x greater than before
+
+# model.mdu['numerics','TurbulenceModel']=1 # 0: dead run.  1: should be 5e-5.
+# model.mdu['physics','Vicoww']=5e-3 # 100x greater than before
+
+# Does a saline IC help at all?
+model.mdu['physics','InitialSalinity']=32.0
 
 model.write()
 model.partition()
