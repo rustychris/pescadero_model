5e142bd9e2575248d75bebc382f99e222b3c27ab
Rusty Holleman <rustychris@gmail.com>
2021-06-17 11:16:55 -0700

    Poking around

--- model/sync_from_farm.sh
+++ model/sync_from_farm.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VERSIONS="v92 v93 v95 v93-restart"
+VERSIONS="v96 v97"
 
 for V in $VERSIONS ; do
 	echo $V
 	rsync -rvPl --size-only -e 'ssh -p2022' --include '*/' --include '*0000.dia' --include '*0000_his.nc' --include '*.py' --include 'flowfm.mdu' --exclude '*' rustyh@farm.cse.ucdavis.edu:src/pescadero/model/model/run_salt_20160520-$V .
 done
 
-VERSIONS="v93-restart"
+VERSIONS="v97"
 
 # Map output:
 for V in $VERSIONS ; do
