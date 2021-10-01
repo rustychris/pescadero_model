3fd32b9a921fe4e8e1c0b0e2ad38d99e7c24cb64
Sophie Munger <47577699+sophunger@users.noreply.github.com>
2021-10-01 16:37:46 -0700

    Shapefile clean up
    - Correct the direction for all the transects (strutures+xsections) to have the correct sign (i.e. positive downstream)
    - Added a weir on the Butano marsh that represent the post built set up

--- grids/pesca_butano_v03/add_bathy_to_grid.py
+++ grids/pesca_butano_v03/add_bathy_to_grid.py
@@ -3,24 +3,24 @@ import numpy as np
 from stompy.grid import unstructured_grid
 from stompy.spatial import field
 from stompy import utils
+import matplotlib.pyplot as plt
 
 ##
 
-bathy_dir="../../../bathy"
+bathy_dir="E:/proj/Pescadero/pescadero_bathy"
 # The as-built needs to be generated
-bathy_asbuilt_fn=os.path.join(bathy_dir,'compiled-dem-asbuilt-20210913-1m.tif')
-bathy_existing_fn=os.path.join(bathy_dir,'compiled-dem-existing-20210913-1m.tif')
+#bathy_asbuilt_fn=os.path.join(bathy_dir,'compiled-dem-asbuilt-20210913-1m.tif')
+bathy_existing_fn=os.path.join(bathy_dir,'compiled-dem-existing-20210913-1m_deep_channel_mod3.tif')
 
 ##
 
-grid_dir="../../../grid"
+grid_dir="Y:/smunger/GitHub/Pescadero/pescadero_grid"
 grid_fn=os.path.join(grid_dir,'quad_tri_v21-edit05.nc')
 g=unstructured_grid.UnstructuredGrid.read_ugrid(grid_fn)
 g.renumber()
 
 ##
-for dem_fn, name in [ (bathy_asbuilt_fn,'asbuilt'),
-                      (bathy_existing_fn,'existing') ]:
+for dem_fn, name in [ (bathy_existing_fn,'existing') ]:
     print("Setting bathymetry for %s"%name)
     if not os.path.exists(dem_fn):
         print("----  DEM file %s doesn't exist ----"%dem_fn)
@@ -56,9 +56,16 @@ for dem_fn, name in [ (bathy_asbuilt_fn,'asbuilt'),
             z_node[n]=edge_data[g.node_to_edges(n),0].min()
         
     g.add_node_field('node_z_bed',z_node,on_exists='overwrite')
-    g.write_ugrid(f'pesca_butano_{name}_bathy.nc',overwrite=True)
+    g.write_ugrid(f'pesca_butano_{name}_bathy_mod3.nc',overwrite=True)
 
     del dem
 
 
+plt.figure(1).clf()
+#at.grid.plot_edges(lw=0.4,color='k')
+g.plot_edges(lw=0.7,color='k',alpha=0.5)
+
+g.plot_cells(labeler='id',alpha=0.2,centroid=True)
+g.plot_edges(labeler='id',label_jitter=0.5)
+g.plot_nodes(labeler='id',label_jitter=0.5)
 
